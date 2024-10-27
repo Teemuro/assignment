92ab755a5b7a5988cf1b965c57d54d264deea7b5
Teemuro <n3rete00@students.oamk.fi>
2024-10-27 18:13:39 +0200

    dropdown menu for products added

--- Assignment/src/App.jsx
+++ Assignment/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Header from './components/ComponentUI';
+import ProductForm from './components/OrderForm';
+
 
 function App() {
 
   return (
     <div>
       <Header title="Welcome to product page"/>
+      <ProductForm/>
 
     </div>
   )};
